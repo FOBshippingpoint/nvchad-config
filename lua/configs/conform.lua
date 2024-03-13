f9e8ec2153b718eea8f9dc44b1c6e15fa9bb68f7
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-03-13 11:46:36 +0800

    feat: add lsp and conform servers

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,9 +1,16 @@
 local options = {
-  lsp_fallback = true,
+	lsp_fallback = true,
 
-  formatters_by_ft = {
-    lua = { "stylua" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "biome" },
+		typescript = { "biome" },
+		css = { "prettier" },
+		html = { "prettier" },
+		go = { "gofmt" },
+		gleam = { "gleam" },
+    python = { "ruff_format" }
+	},
 }
 
 require("conform").setup(options)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,46 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("configs.conform")
+		end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      git = { enable = true },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			git = { enable = true },
+		},
+	},
+
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				-- lua stuff
+				"lua-language-server",
+				"stylua",
+				-- web dev stuff
+				"css-lsp",
+				"html-lsp",
+				"prettier",
+				"biome",
+				"deno",
+				-- other stuff
+				"gopls",
+				"gleam",
+				-- python stuff
+				"ruff",
+				"ruff_lsp",
+			},
+		},
+	},
 }
