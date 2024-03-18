47969f0b378c9f4a4eb8097baaa24508fc2d7681
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-03-18 23:24:11 +0800

    feat: add nushell config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,14 +3,28 @@ local options = {
 
 	formatters_by_ft = {
 		lua = { "stylua" },
-		javascript = { "biome" },
-		typescript = { "biome" },
+		javascript = { "biome", "prettier" },
+		javascriptreact = { "biome", "prettier" },
+		typescript = { "biome", "prettier" },
+		typescriptreact = { "biome", "prettier" },
 		css = { "prettier" },
 		html = { "prettier" },
 		go = { "gofmt" },
 		gleam = { "gleam" },
-    python = { "ruff_format" }
+		python = { "ruff_format" },
 	},
+
+	-- format_on_save = {
+	-- 	-- I recommend these options. See :help conform.format for details.
+	-- 	lsp_fallback = true,
+	-- 	timeout_ms = 500,
+	-- },
+	-- -- If this is set, Conform will run the formatter asynchronously after save.
+	-- -- It will pass the table to conform.format().
+	-- -- This can also be a function that returns the table.
+	-- format_after_save = {
+	-- 	lsp_fallback = true,
+	-- },
 }
 
 require("conform").setup(options)
--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,11 @@
 require "nvchad.options"
 
 -- add yours here!
+
+-- set shell as NuShell
+vim.opt.shell = "nu"
+vim.opt.shellcmdflag = "-c"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
+
+vim.opt.relativenumber = true
